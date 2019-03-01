55c9e5d8d8a5b7c1f79d4dbe1ef7640bb2c0b98f
Pierre-Louis242 <plpicard@hotmail.fr>
2019-03-01 17:22:28 +0100

    Almos finished V.1.01

--- Assets/main.js
+++ Assets/main.js
@@ -3,14 +3,14 @@ window.onload = () => {
 
     let map1 = document.querySelector("#changementMap1")
     map1.addEventListener("click", function(){
-        map.src = "Images/map1.png"
+        map.src = "Assets/Images/map1.png"
     })
 
     document.querySelector("#changementMap2").addEventListener("click", function(){
-        map.src = "Images/map2.png"
+        map.src = "Assets/Images/map2.png"
     })
 
     document.querySelector("#changementMap3").addEventListener("click", function(){
-        map.src = "Images/map3.png"
+        map.src = "Assets/Images/map3.png"
     })
 }
